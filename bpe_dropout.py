060acc394b5b2ae95e94fc3ea18eb4961b5a8b06
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2025-03-29 11:30:52 +0100

    notebooks, not py scripts

--- bpe_dropout.py
+++
@@ -1,4 +0,0 @@
-'''
-BPE-Dropout: Simple and Effective Subword Regularization
-paper: https://arxiv.org/pdf/1910.13267
-'''
--- layer-norm.py
+++
@@ -1,6 +0,0 @@
-'''
-LayerNorm
-paper: https://arxiv.org/pdf/1607.06450
-'''
-
-# goal: simple MLP and then implemenet a LayerNorm layer custom via paper
